909fce994e51236c3336ca29276956169bcf0632
trazalem <isdrael4590@gmail.com>
2023-07-24 16:01:06 -0500

    fisrt comment

---
+++ dist/config/db.php
@@ -0,0 +1,17 @@
+<?php
+  //  <?php
+  $servername="localhost";
+  $username="root";
+  $password="19900405";
+  $dbname="CODESRUMED";
+  
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  
+  
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+  ?>
+?>  
